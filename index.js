ae502c6be37cfa88f0c41137ff4076d0504971b1
Dustin Deus <deusdustin@gmail.com>
2018-03-27 21:49:36 +0200

    improve eslint config

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 module.exports = {
   extends: ["standard", "prettier", "prettier/standard"],
@@ -6,11 +6,25 @@ module.exports = {
   rules: {
     "mocha/no-exclusive-tests": "error",
     "space-before-function-paren": "off",
-    "prettier/prettier": "error"
+    "prettier/prettier": "error",
+
+    "promise/catch-or-return": "error",
+    "promise/no-return-wrap": "error",
+    "promise/param-names": "error",
+
+    "node/no-unsupported-features": "error",
+    "node/no-missing-require": "error",
+    "node/no-unpublished-bin": "error",
+    "node/shebang": "error",
+    "node/no-unpublished-require": "error",
+    "node/no-deprecated-api": "error"
+  },
+  parserOptions: {
+    ecmaVersion: 9
   },
   env: {
     es6: true,
     node: true,
     mocha: true
   }
-}
+};
